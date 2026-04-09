cfe74aeb8fc942328a989743723cb7f2ba3023eb
Arian Gill <ariangill208@yahoo.com>
2026-04-09 23:27:18 +0100

    uploaded Main.java

---
+++ Complexity and searching/Main.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 64, 1001, 28, 39, -99, 123, 619, 71};
+        
+        Arrays.sort(numbers);
+
+        int index = binarySearch(numbers, 619);
+
+        if (index != -1) {
+            System.out.println("Target has been found");
+        }
+        else {
+            System.out.println("Target has not been found");
+        }
+    }
+
+    static int binarySearch(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else if (nums[mid] < target) left = mid + 1;
+        }
+        
+        return -1;
+    }
+}
